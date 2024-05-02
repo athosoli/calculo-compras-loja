92db51b51d0d3ab058f15e8f06cff117bbbcb145
athos <athos@ciandt.com>
2024-05-02 16:24:03 -0300

    funcao-Calcular-Total

--- src/compras/compras.js
+++ src/compras/compras.js
@@ -1,24 +1,29 @@
 const calcularTotal = (ferramentas, comprar) => {
+
+    if (ferramentas.length === 0 || comprar.length === 0) {
+        throw new Error("Ambas as listas precisam ter ao menos um item.")
+    }
+
     let total = 0
-    for (let i = 0; i < ferramentas.length; i++) {
-        for (let j = 0; j < comprar.length; j++) {
+    let encontraFerramenta = false
+
+    for (let j = 0; j < comprar.length; j++) {
+        
+        encontraFerramenta = false
+
+        for (let i = 0; i < ferramentas.length; i++) {
             if (ferramentas[i].nome === comprar[j]) {
                 total += ferramentas[i].preco
+                encontraFerramenta = true
                 break
             }
         }
+        if (!encontraFerramenta) {
+             throw new Error("Nenhuma ferramenta desejada encontrada.")
+        }
     }
+
     return `O valor a pagar pelas ferramentas (${comprar.join(', ')}) é R$ ${total.toFixed(2)}`
 }
-
-const ferramentas = [
-    { nome: "UFT", preco: 100, fabricante: "OpenText" },
-    { nome: "TestComplete", preco: 200, fabricante: "Smartbear" },
-    { nome: "TOSCA", preco: 300, fabricante: "Tricents" }
-];
-
-const comprar = [ "UFT", "TOSCA" ]
-
-console.log(calcularTotal(ferramentas, comprar))
 
 module.exports = { calcularTotal }
